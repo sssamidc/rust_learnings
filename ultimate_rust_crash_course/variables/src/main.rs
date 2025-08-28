cb77b7b800facae9542a88f5a08604a49a81d784
Subhra S Sarkar <subhra.sarkar@atimotors.com>
2025-08-28 18:29:26 +0530

    [Section-02] Finished part # 1 of the exercise for a-variables

---
+++ ultimate_rust_crash_course/variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+
+    // Variable declarations
+    let missiles: i32 = 8;
+    let ready: i32 = 2;
+
+    println!("Firing {} of my {} missiles ...", ready, missiles);
+}
