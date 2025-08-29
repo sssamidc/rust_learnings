17b0f4b8d1013ce7d8c65002a7def87d63fb10c2
Subhra S Sarkar <subhra.sarkar@atimotors.com>
2025-08-29 17:52:42 +0530

    [Section-02] Finished part # 2 and the extra challenges section of the exercise for a-variables

--- ultimate_rust_crash_course/variables/src/main.rs
+++ ultimate_rust_crash_course/variables/src/main.rs
@@ -1,9 +1,22 @@
+// Constant declarations
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
     println!("Hello, world!");
 
     // Variable declarations
-    let missiles: i32 = 8;
-    let ready: i32 = 2;
+    // let test: i32 = 1234;
+    // READY_AMOUNT = 1;
+    // let mut missiles: i32 = STARTING_MISSILES;
+    // let ready: i32 = READY_AMOUNT;
+
+    // Pattern based declaration
+    // let (mut missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+    let (missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
 
     println!("Firing {} of my {} missiles ...", ready, missiles);
+
+    // missiles = missiles - ready;
+    println!("{} missiles left", missiles - ready);
 }
